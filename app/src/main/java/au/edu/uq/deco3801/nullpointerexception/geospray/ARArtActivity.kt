a90702e9ee52dea89711b69a48f0d9349c154f78
Lachlan Slape <slapelachie@gmail.com>
2023-09-07 10:33:00 +1000

    add second activity for ARArt and update perms

--- app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARArtActivity.kt
+++ app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARArtActivity.kt
@@ -1,33 +1,28 @@
 package au.edu.uq.deco3801.nullpointerexception.geospray
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import au.edu.uq.deco3801.nullpointerexception.geospray.ui.theme.GeoSprayTheme
 
-class MainActivity : ComponentActivity() {
+class ARArtActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GeoSprayTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainContent()
+                    Greeting2("Android")
                 }
             }
         }
@@ -35,25 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent () {
-    val content = LocalContext.current
-
-    Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = { content.startActivity(Intent(content, ARArtActivity::class.java)) }) {
-            Text(text = "AR Test")
-        }
-    }
-
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainContentPreview() {
+fun GreetingPreview2() {
     GeoSprayTheme {
-        MainContent()
+        Greeting2("Android")
     }
 }
