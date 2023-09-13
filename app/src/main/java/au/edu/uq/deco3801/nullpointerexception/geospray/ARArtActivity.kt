f8c9cb6efadc7e7c356aebb9d85aafbfe1d8868e
Lachlan Slape <slapelachie@gmail.com>
2023-09-13 15:04:32 +1000

    get camera displayed

--- app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARArtActivity.kt
+++ app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARArtActivity.kt
@@ -3,17 +3,22 @@ package au.edu.uq.deco3801.nullpointerexception.geospray
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import au.edu.uq.deco3801.nullpointerexception.geospray.helpers.DisplayRotationHelper
 
 class ARArtActivity : ComponentActivity() {
     private lateinit var arSurfaceView: ARSurfaceView
+    private lateinit var displayRotationHelper: DisplayRotationHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        arSurfaceView = ARSurfaceView(this)
+        displayRotationHelper = DisplayRotationHelper(this)
+        arSurfaceView = ARSurfaceView(this, displayRotationHelper)
+
         arSurfaceView.preserveEGLContextOnPause = true
         arSurfaceView.setEGLContextClientVersion(2)
         arSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0)
-        arSurfaceView.setRenderer(ARRenderer(this))
+        arSurfaceView.setRenderer(arSurfaceView)
         arSurfaceView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
         arSurfaceView.setWillNotDraw(false)
 
--- app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARSurfaceView.kt
+++ app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/ARSurfaceView.kt
@@ -2,13 +2,30 @@ package au.edu.uq.deco3801.nullpointerexception.geospray
 
 import android.app.Activity
 import android.content.Context
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.util.Log
+import au.edu.uq.deco3801.nullpointerexception.geospray.helpers.DisplayRotationHelper
 import au.edu.uq.deco3801.nullpointerexception.geospray.helpers.GeoSprayPermissionsHelper
+import au.edu.uq.deco3801.nullpointerexception.geospray.rendering.BackgroundRenderer
+import au.edu.uq.deco3801.nullpointerexception.geospray.rendering.PlaneRenderer
+import au.edu.uq.deco3801.nullpointerexception.geospray.rendering.PointCloudRenderer
 import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Camera
+import com.google.ar.core.Config
+import com.google.ar.core.Frame
 import com.google.ar.core.Session
+import com.google.ar.core.exceptions.CameraNotAvailableException
+import java.io.IOException
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class ARSurfaceView(context: Context) : GLSurfaceView(context) {
+class ARSurfaceView(context: Context, private val displayRotationHelper: DisplayRotationHelper) :
+    GLSurfaceView(context), GLSurfaceView.Renderer{
+    private val backgroundRenderer: BackgroundRenderer = BackgroundRenderer()
+    private val planeRenderer: PlaneRenderer = PlaneRenderer()
+    private val pointCloudRenderer: PointCloudRenderer = PointCloudRenderer()
+
     private lateinit var session: Session
 
     private fun requestARCoreInstallation(context: Context) {
@@ -30,11 +47,12 @@ class ARSurfaceView(context: Context) : GLSurfaceView(context) {
                         }
                     }
                 } catch (e: Exception) {
-                    // TODO: Handle exception
+                    Log.e(TAG, "Some error", e)
                 }
             }
 
             else -> {
+                Log.e(TAG, "ARCore is not supported on this device")
                 // TODO: ARCore is not supported on this device
             }
         }
@@ -42,10 +60,7 @@ class ARSurfaceView(context: Context) : GLSurfaceView(context) {
 
     override fun onResume() {
         super.onResume()
-
-        Log.d("FOO123", "test")
         if (!::session.isInitialized) {
-            Log.d("FOO123", "is not initialised")
             val exception: Exception? = null
             val message: String? = null
 
@@ -55,13 +70,86 @@ class ARSurfaceView(context: Context) : GLSurfaceView(context) {
                 GeoSprayPermissionsHelper.requestPermissions(context)
             }
 
-            // TODO: Create new session
+            try {
+                Log.d(TAG, "Creating new session")
+                // Create new session
+                session = Session(context)
 
-            // TODO: Configure the new session
+                // Configure the new session
+                val config = Config(session)
+                config.cloudAnchorMode = Config.CloudAnchorMode.ENABLED
+                config.focusMode = Config.FocusMode.AUTO
+                session.configure(config)
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to create session", e)
+            }
+
 
         }
 
-        // TODO: Try resumming session
+        try {
+            session.resume()
+        } catch (e: CameraNotAvailableException) {
+            // TODO: Show error
+            Log.e(TAG, "Camera is not available", e)
+            return
+        }
+        displayRotationHelper.onResume()
     }
 
+    override fun onPause() {
+        super.onPause()
+        displayRotationHelper.onPause()
+        session.pause()
+    }
+
+    override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+
+        try {
+            backgroundRenderer.createOnGlThread(context)
+            planeRenderer.createOnGlThread(context, "models/trigrid.png")
+            pointCloudRenderer.createOnGlThread(context)
+        } catch (e: IOException) {
+            Log.e(TAG, "Failed to read an asset file", e)
+        }
+    }
+
+    override fun onSurfaceChanged(p0: GL10?, p1: Int, p2: Int) {
+        displayRotationHelper.onSurfaceChanged(width, height)
+        GLES20.glViewport(0, 0, width, height)
+    }
+
+    override fun onDrawFrame(p0: GL10?) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+
+        if (!::session.isInitialized) return
+
+        try {
+            displayRotationHelper.updateSessionIfNeeded(session)
+
+            session.setCameraTextureName(backgroundRenderer.textureId)
+
+            val frame: Frame = session.update()
+            val camera: Camera = frame.camera
+
+            // TODO: Handle tap
+
+            backgroundRenderer.draw(frame)
+
+            // TODO: Tracking state
+
+            // TODO: Camera matrix
+
+            // TODO: Point cloud
+
+            // TODO: Draw planes
+        } catch (e: Exception) {
+            Log.e(TAG, "Error on session", e)
+        }
+    }
+
+    companion object {
+        private const val TAG: String = "ARRenderer"
+    }
 }
