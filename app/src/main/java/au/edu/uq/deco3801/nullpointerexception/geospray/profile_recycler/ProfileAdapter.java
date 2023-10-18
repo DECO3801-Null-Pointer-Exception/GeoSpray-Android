89cd3210b8615998817f12b45cac5a51f1c53b97
Raymond Dufty <80143566+RaymondDufty@users.noreply.github.com>
2023-10-19 00:14:54 +1000

    Add docstrings and comments.

--- app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/profile_recycler/ProfileAdapter.java
+++ app/src/main/java/au/edu/uq/deco3801/nullpointerexception/geospray/profile_recycler/ProfileAdapter.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import au.edu.uq.deco3801.nullpointerexception.geospray.R;
 import au.edu.uq.deco3801.nullpointerexception.geospray.rendering.GalleryImage;
 
+/**
+ * Class used to show images on the user's profile page in a scrollable fashion.
+ */
 public class ProfileAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final int VIEW_TYPE_ITEM = 0;
     private final int VIEW_TYPE_LOADING = 1;
@@ -21,6 +24,12 @@ public class ProfileAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     private Context context;
     private ArrayList<GalleryImage> profileImages;
 
+    /**
+     * Constructor for a ProfileAdapter.
+     *
+     * @param context The Context of this Adapter.
+     * @param galleryImages The list of images this Adapter is to display.
+     */
     public ProfileAdapter(Context context, ArrayList<GalleryImage> galleryImages) {
         this.context = context;
         this.profileImages = galleryImages;
@@ -50,10 +59,19 @@ public class ProfileAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         return profileImages.size();
     }
 
+    /**
+     * Returns the type of the image at a given position.
+     *
+     * @param position Position to query.
+     * @return The image's type.
+     */
     public int getItemViewType(int position) {
         return profileImages.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
     }
 
+    /**
+     * Class representing a container for a View's images.
+     */
     public class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView image;
 
@@ -62,5 +80,4 @@ public class ProfileAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             image = itemView.findViewById(R.id.profile_image);
         }
     }
-
 }
